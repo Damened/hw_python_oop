12833b334b4e60adc579ba45280fe2abe0541919
Ragnar Lodbrok <ragnar@MacBook-Air-Ragnar.local>
2022-12-28 14:54:46 +0500

    finished code ((2 attempt) without edits

--- homework.py
+++ homework.py
@@ -62,6 +62,13 @@ class Running(Training):
     CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
     CALORIES_MEAN_SPEED_SHIFT: float = 1.79
 
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 ) -> None:
+        super().__init__(action, duration, weight)
+
     def get_spent_calories(self) -> float:
         return ((self.CALORIES_MEAN_SPEED_MULTIPLIER
                 * self.get_mean_speed() + self.CALORIES_MEAN_SPEED_SHIFT)
